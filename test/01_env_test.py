b632eda61f247b8e32f180cdf937a3bfc0eefd45
cyt <734039653@qq.com>
2026-09-14 17:58:20 +0800

    完成项目初始化搭建。

---
+++ test/01_env_test.py
@@ -0,0 +1,10 @@
+# test/01_env_test.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv(
+    override=True
+)
+
+print(os.environ.get("BGE_M3_PATH"))
+# load_dotenv(override=True) → 输出 dotenv_val（.env覆盖系统）
